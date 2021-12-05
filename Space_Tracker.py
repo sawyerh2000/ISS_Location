65dea03900c6f3f59f8979695debf798ebefbb58
Sawyer Hughes <sawyerh2000@gmail.com>
2021-12-04 19:05:08 -0500

    comments

--- Space_Tracker.py
+++ Space_Tracker.py
@@ -6,6 +6,7 @@ import http.client, urllib.parse
 
 ACCESS_KEY = '8d292a338a1ce7bcfb8f14cda2777f84'
 
+#Function to get the number and data of astronauts currently in space 
 def get_astros():
     json_response = requests.get('http://api.open-notify.org/astros.json') #send GET request to api endpoint contain json data on current astronauts in space
 
@@ -15,23 +16,24 @@ def get_astros():
 
     print(dict1['number']) #print number of astronauts in space currently
 
+
+#Function to get location of ISS in latitude and longitude 
 def iss_location():
-    json_response = requests.get('http://api.open-notify.org/iss-now.json')
+    json_response = requests.get('http://api.open-notify.org/iss-now.json') #send GET request to endpoint 
 
-    str_res = json.dumps(json_response.json(), indent=4, sort_keys=True)
+    str_res = json.dumps(json_response.json(), indent=4, sort_keys=True) #represent json data as string
 
-    dict1 = json.loads(str_res)
+    dict1 = json.loads(str_res) #load string into dictionary object
 
-    str1=dict1['iss_position']['latitude'] + ',' + dict1['iss_position']['longitude']
+    str1=dict1['iss_position']['latitude'] + ',' + dict1['iss_position']['longitude'] #place latitude and longitude from dict into string
 
-    print(str1)
-    return str1
+    return str1 #return string
 
 
-    
-
+#Function to use coordinates from iss_location() to give more readable format of ISS location. 
 def country(coords):
-    conn = http.client.HTTPConnection('api.positionstack.com')
+
+    conn = http.client.HTTPConnection('api.positionstack.com')  #connect to server
 
     params = urllib.parse.urlencode({
         'access_key' : ACCESS_KEY,
